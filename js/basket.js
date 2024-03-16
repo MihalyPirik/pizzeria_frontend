294519581fe12fe93cbbd671d8e22c7c55e54ab4
Mihaly Pirik <misipirik@gmail.com>
2024-03-16 16:33:10 +0100

    add orders table

---
+++ js/basket.js
@@ -0,0 +1,83 @@
+import { createApiEndpoint, handleLogout, clearDisplayDiv, getCookie } from './functions.js';
+
+const displayDiv = document.querySelector('#displayDiv');
+const userNameElement = document.querySelector('#user-name');
+const logoutButton = document.querySelector('#logout-button');
+
+const orderApiUrl = createApiEndpoint(`orders`);
+const userApiUrl = createApiEndpoint("user");
+
+const userToken = getCookie('userToken');
+let userData;
+let orderData;
+
+logoutButton.addEventListener('click', handleLogout);
+
+fetch(userApiUrl, {
+  method: 'GET',
+  headers: {
+    'Authorization': `Bearer ${userToken}`
+  }
+})
+  .then(response => response.json())
+  .then(data => {
+    userData = data;
+
+    const userName = userData.name;
+    userNameElement.textContent = userName;
+
+    fetch(orderApiUrl, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${userToken}`
+      }
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Hiba történt a kérés során: ' + response.status);
+        }
+        return response.json();
+      })
+      .then(data => {
+        orderData = data[0].order;
+        let table = generateTable(orderData);
+        displayDiv.appendChild(table);
+      })
+      .catch(error => {
+        console.error('Hiba történt:', error);
+      });
+  })
+  .catch(error => {
+    console.error('Hiba történt az adatok lekérése közben:', error);
+  });
+
+function generateTable(orderData) {
+  let tableContainer = document.createElement('table');
+  tableContainer.classList.add('container');
+
+  // Fejléc létrehozása
+  let thead = document.createElement('thead');
+  let headRow = document.createElement('tr');
+  ['Mennyiség', 'Megnevezés', 'Ár', 'Összesen'].forEach((headerText) => {
+    let th = document.createElement('th');
+    th.textContent = headerText;
+    headRow.appendChild(th);
+  });
+  thead.appendChild(headRow);
+  tableContainer.appendChild(thead);
+
+  // Tartalom létrehozása
+  let tbody = document.createElement('tbody');
+  orderData.forEach((rowData) => {
+    let tr = document.createElement('tr');
+    rowData.forEach((data) => {
+      let td = document.createElement('td');
+      td.textContent = data;
+      tr.appendChild(td);
+    });
+    tbody.appendChild(tr);
+  });
+  tableContainer.appendChild(tbody);
+
+  return tableContainer;
+}
